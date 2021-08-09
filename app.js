40d37449ed0884396c140ec4b1a97adfacf48861
kevinSarli <kevin.sarli99@gmail.com>
2021-08-09 00:37:25 -0300

    mercado liebre

---
+++ app.js
@@ -0,0 +1,12 @@
+const express = require('express');
+const app = express();
+const path = require('path');
+let port = 3030;
+
+app.use(express.static('public'));
+
+app.get('', (req,res) => res.sendFile(path.join(__dirname, 'views', 'home.html')));
+app.get('/login', (req,res) => res.sendFile(path.join(__dirname, 'views', 'login.html')));
+app.get('/register', (req,res) => res.sendFile(path.join(__dirname, 'views', 'register.html')));
+
+app.listen(puerto, () => console.log('Servidor corriendo en http://localhost:' + port));
